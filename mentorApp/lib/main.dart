207bef6f9ee0b9bc541d9fb26ead58b1d748103e
Aysha Araien <aaraien@ualberta.ca>
2020-05-01 17:41:45 -0600

    chore: added AAA - added a comment

--- mentorApp/lib/main.dart
+++ mentorApp/lib/main.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
+//AAA
+
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
