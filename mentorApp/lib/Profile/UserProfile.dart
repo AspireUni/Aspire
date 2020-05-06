e73334670adc4644dbbbda6bc17a526975d6dfdf
Helen Gezahegn <gezahegn@ualberta.ca>
2020-05-06 16:30:19 -0600

    feat: nav bar and screen navigation (#11)
    - added bottom navigation bar
    - created placeholder screens and nav items for profile, home, and chat screens
    - created an AppController to maintain the screen navigation state
    - set default navigation item to the center home icon
    - upgraded nav bar styling using curved navigation bar package

---
+++ mentorApp/lib/Profile/UserProfile.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+// TODO: Update this placeholder widget
+class UserProfile extends StatelessWidget {
+  const UserProfile({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Container(
+        child: Center(
+          child: Text("Profile", style: TextStyle(color: Colors.white, fontSize: 30.0))
+        )
+      )
+    );
+  }
+}
