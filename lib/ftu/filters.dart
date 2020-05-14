0e2b1b55c3c436949865668b4073894942b0039c
Helen Gezahegn <gezahegn@ualberta.ca>
2020-05-14 01:00:26 -0600

    chore: update to snake_case

---
+++ lib/ftu/filters.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+// TODO: Update this placeholder widget
+class Filters extends StatelessWidget {
+  const Filters({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: new AppBar(
+        title: new Text("Select up to 3 options", style: TextStyle(color: Colors.white, fontSize: 20.0))
+      ),
+      body: InkWell(
+        onTap: (){
+          //Scaffold.of(context).showSnackBar(SnackBar(
+          //content: Text('Tap'),));
+          Scaffold.of(context);
+        },
+        child: Container(
+          padding: EdgeInsets.all(12.0),
+          child: Text('Flat Button', style: TextStyle(color: Colors.white)),
+      )
+      )
+      );
+  }
+}
