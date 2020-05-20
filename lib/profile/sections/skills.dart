9adfa76e22860227ee075a95b797743cab903198
Eivenlour David <eivenlou@ualberta.ca>
2020-05-19 20:57:08 -0600

    feat: user profile view (#53)
    * feat: user profile view
    
    * refactor: dummy data
    
    * refactor: sections icons
    
    * fix: header border
    
    * fix: code review comments
    - build widgets indentation
    - section icon background color
    - creating sections folders for skills, bio, info
    
    * fix: more indentations

---
+++ lib/profile/sections/skills.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+// TODO: Update this placeholder widget
+class Skills extends StatelessWidget {  
+  const Skills ({Key key}) : super(key: key);
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Container(
+        child: Center(
+          child: Text("Skills", style: TextStyle(color: Colors.white, fontSize: 30.0))
+        )
+      )
+    );
+  }
+}
