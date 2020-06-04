bd3437239fde368af3f41fd3b203e17595d81dc0
Nayan Prakash <nayanprakash2@gmail.com>
2020-06-04 13:13:41 -0600

    feat: add timestamp on last message

--- lib/chat/chat_messenger.dart
+++ lib/chat/chat_messenger.dart
@@ -1,35 +1,43 @@
 import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 const dummyData = [
   {
     "text": "Hello",
     "isSent": true,
+    "timestamp": '2020-06-04 09:36'
   },
   {
     "text": "it's me",
     "isSent": false,
+    "timestamp": '2020-06-04 09:36'
   },
   {
     "text": "I was wondering if after all these years you'd like to meet",
     "isSent": true,
+    "timestamp": '2020-06-04 09:37'
   },
   {
     "text": "to go over?",
     "isSent": false,
+    "timestamp": '2020-06-04 09:38'
   },
   {
     "text": "everything.",
     "isSent": true,
+    "timestamp": '2020-06-04 09:39'
   },
   {
     "text": "they say that time's supposed to heal ya",
     "isSent": true,
+    "timestamp": '2020-06-04 09:41'
   },
   {
     "text": "but I ain't done much healing 😠",
     "isSent": true,
+    "timestamp": '2020-06-04 09:42'
   },
 ];
 
@@ -37,14 +45,15 @@ class ChatMessengerState extends State<ChatMessenger> {
   String recipient;
   List<Object> messages = [...dummyData];
 
-  void addMessage (message, isSent) {
-    setState(() {messages = [...messages, {"text": message, "isSent": isSent}];});
+  void addMessage (message, timestamp, isSent) {
+    setState(() {messages = [...messages, {"text": message, "isSent": isSent, "timestamp": timestamp}];});
   }
 
   void submitMessage (String value) {
     textInputController.text = "";
     if (value != "") {
-      addMessage(value, true);
+      String timestamp = DateFormat('yyyy-MM-dd kk:mm').format(DateTime.now());
+      addMessage(value, timestamp, true);
       SystemSound.play(SystemSoundType.click);
     }
   }
@@ -54,6 +63,26 @@ class ChatMessengerState extends State<ChatMessenger> {
   List<Widget> buildMessages() {
     List<Widget> messagesList = new List<Widget>();
     for (int i = messages.length - 1; i >= 0; i--) {
+      if (i == messages.length - 1) {
+        messagesList.add(
+          Align(
+            alignment: (messages[i] as Map)["isSent"] ? Alignment.centerRight : Alignment.centerLeft,
+            child:  Container(
+              margin: (messages[i] as Map)["isSent"] ? EdgeInsets.fromLTRB(50.0, 0.0, 10.0, 10.0) : EdgeInsets.fromLTRB(10.0, 10.0, 50.0, 10.0),
+              child: Text (
+                DateFormat('jm').format(DateFormat('yyyy-MM-dd kk:mm').parse((messages[i] as Map)["timestamp"])),
+                style: GoogleFonts.muli(
+                  textStyle: TextStyle(
+                    color: Colors.grey, 
+                    letterSpacing: .5, 
+                    fontSize: 11.0, 
+                  )
+                )
+              )
+            )
+          )
+        );
+      }
       messagesList.add(
         Align(
           alignment: (messages[i] as Map)["isSent"] ? Alignment.centerRight : Alignment.centerLeft,
