4c536e217d4ddb226a5ee6c938b05044f70e5c16
AyshaAraien <aaraien@ualberta.ca>
2020-06-15 15:00:02 -0600

    feat: fix import statement

--- lib/ftu/filters.dart
+++ lib/ftu/filters.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'dart:async';
 import 'dart:convert';
-import 'package:mentorApp/ftu/parsing.dart';
+import'../ftu/parsing.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class FiltersPage extends StatefulWidget {
