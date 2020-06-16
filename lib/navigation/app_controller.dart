df0399b8e77341a419943205172c123284249938
Helen Gezahegn <gezahegn@ualberta.ca>
2020-06-16 17:04:20 -0600

    fix: import statements for app_controller

--- lib/navigation/app_controller.dart
+++ lib/navigation/app_controller.dart
@@ -1,9 +1,10 @@
 import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
-import 'package:aspire/constants/navigation_constants.dart';
-import "../chat/chat.dart";
+
+import '../chat/chat.dart';
+import '../constants/navigation_constants.dart';
 import '../pairings/pairings.dart';
-import "../profile/user_profile.dart";
+import '../profile/user_profile.dart';
 
 class AppController extends StatefulWidget {
   @override _MyAppState createState() => _MyAppState();
