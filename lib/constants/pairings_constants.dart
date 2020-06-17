581e4624ea2027574ce95e6ee0f01adaeb101fcd
Helen Gezahegn <gezahegn@ualberta.ca>
2020-06-17 14:53:26 -0600

    feat: card component + swipe gesture (#69)
    - added flutter_tinkercard 
    - alphabetized dependencies in pubspec
    - created pairings_card component that contains card + swipe animation
    - deleted unused code in pairings

---
+++ lib/constants/pairings_constants.dart
@@ -0,0 +1,10 @@
+const String pairingsHeaderTitle = "Pairings";
+const String pairingsSubtitle = "Browse your matches";
+
+List<String> dummyImages = const [
+  "images/PairingsBg.png",
+  "images/DefaultBg.png",
+  "images/PairingsBg.png",
+  "images/DefaultBg.png", 
+  "images/PairingsBg.png"
+];
