ec3c6c912eeb229828f1f89562c40df8bf9606c5
Xinyue Xiang <xxiang2@ualberta.ca>
2020-06-18 11:03:50 -0600

    feature: add the signup and login part in the application

---
+++ lib/ftu/homepage.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import './authentication.dart';
+import 'package:firebase_database/firebase_database.dart';
+//import './todo.dart';
+import 'dart:async';
+
+class HomePage extends StatefulWidget {
+  HomePage({Key key, this.auth, this.userId, this.logoutCallback})
+      : super(key: key);
+
+  final BaseAuth auth;
+  final VoidCallback logoutCallback;
+  final String userId;
+
+  @override
+  State<StatefulWidget> createState() => new _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  
+
+  final FirebaseDatabase _database = FirebaseDatabase.instance;
+  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text("HomePage"),
+      ),
+      body: new Container(
+        child: new Text("Hello World"),
+      ),
+    );
+  }
+
+  signOut() async {
+    try {
+      await widget.auth.signOut();
+      widget.logoutCallback();
+    } catch (e) {
+      print(e);
+    }
+  }
+
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,43 @@
-import 'package:device_preview/device_preview.dart';
+// import 'package:device_preview/device_preview.dart';
+// import 'package:flutter/material.dart';
+// import "./navigation/root.dart";
+// void main() => {
+//   runApp(
+//     DevicePreview(
+//       enabled: true,
+//       areSettingsEnabled: true,  
+//       builder: (context) => App()
+//     )
+//   )
+// }; 
+
+// class App extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       builder: DevicePreview.appBuilder,
+//       home: Root()
+//     );
+//   }
+// }
+
 import 'package:flutter/material.dart';
-import "./navigation/root.dart";
-void main() => {
-  runApp(
-    DevicePreview(
-      enabled: true,
-      areSettingsEnabled: true,  
-      builder: (context) => App()
-    )
-  )
-}; 
+import './FTU/authentication.dart';
+import './FTU/rootpage.dart';
+
+void main() {
+  runApp(new App());
+}
 
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      builder: DevicePreview.appBuilder,
-      home: Root()
-    );
+    return new MaterialApp(
+        title: 'Welcome to Aspire',
+        debugShowCheckedModeBanner: false,
+        theme: new ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: new RootPage(auth: new Auth()));
   }
 }
-
