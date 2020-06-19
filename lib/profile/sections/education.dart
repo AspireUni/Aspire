147acf39d52a87e672e87c9d4146cd1c0b2effe7
Helen Gezahegn <gezahegn@ualberta.ca>
2020-06-18 21:25:54 -0600

    fix: prefer_relative_imports errors (#81)
    - delete `prefer_relative_imports` from errors ignore list
    - fix all violations of this lint rule

--- lib/profile/sections/education.dart
+++ lib/profile/sections/education.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:aspire/constants/profile_constants.dart';
+
+import '../../constants/profile_constants.dart';
 import './section.dart';
 
  
