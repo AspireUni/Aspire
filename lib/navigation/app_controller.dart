185a7d527a36ab31e480b926f3653f5332c2aacf
Helen Gezahegn <gezahegn@ualberta.ca>
2020-07-08 17:48:12 -0600

    chore: delete unused import

--- lib/navigation/app_controller.dart
+++ lib/navigation/app_controller.dart
@@ -1,5 +1,4 @@
 import 'package:curved_navigation_bar/curved_navigation_bar.dart';
-import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 
 import '../FTU/authentication.dart';
