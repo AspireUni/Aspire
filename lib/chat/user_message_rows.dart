a369b40b3b5e3af02dab5ada98d6079e08ca5450
Helen Gezahegn <gezahegn@ualberta.ca>
2020-07-22 20:42:16 -0600

    fix: define mockPeerId - defined mockPeerId in chat_constants.dart - delete unused import statements

--- lib/chat/user_message_rows.dart
+++ lib/chat/user_message_rows.dart
@@ -1,6 +1,5 @@
 import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter/material.dart';
-import 'package:mentorApp/constants/chat_constants.dart';
 
 import '../constants/chat_constants.dart';
 import './chat_messenger.dart';
