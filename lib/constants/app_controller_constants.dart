b41162f5d0d8145fcb70ab60d1ab5edd7f305235
eivenlour <eivenlou@ualberta.ca>
2020-07-25 19:20:22 -0600

    fix: bottom nav icons

--- lib/constants/app_controller_constants.dart
+++ lib/constants/app_controller_constants.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import '../icons/aspire_icons.dart';
 
 // Nav item icons
 const navItemIcons = [
-  Icons.account_circle, 
-  Icons.home, 
-  Icons.message
+  AspireIcons.profile,
+  AspireIcons.pairings,
+  AspireIcons.chat
 ];
 
 const Color navItemColor = Color(0xFF44CBB9);
