eaa0002766c95846faffbfe4e7a68554128ca1c7
Helen Gezahegn <gezahegn@ualberta.ca>
2020-07-25 23:50:19 -0600

    feat: fade-in splash screen + refactor root (#104)
    - added [custom_splash](https://pub.dev/packages/custom_splash)
    - fades-in aspire logo on app launch
    - alphabetized packages in `pubspec.yaml`
    - replace `root.dart` with refactored `rootpage.dart`

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,9 @@ import 'package:redux_persist/redux_persist.dart';
 import 'package:redux_persist_flutter/redux_persist_flutter.dart';
 
 import './constants/signuplogin_constants.dart';
+import './constants/theme_constants.dart';
 import './models/models.dart';
+import './navigation/app_controller.dart';
 import './navigation/root.dart';
 import './reducers/reducers.dart';
 
@@ -56,10 +58,15 @@ class App extends StatelessWidget {
     return MaterialApp(
       title: welcomeMessage,
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+      theme: ThemeData(	
+        primaryColor: primaryColor,	
+        accentColor: accentColor,	
       ),
-      home: Root()
+      initialRoute: '/',
+      routes: {
+        '/': (context) => Root(),
+        '/AppController': (context) => AppController()
+      }
     );
   }
 }
