ba710eb6f575184ab3d2ac6c1c991f5010a87512
Eivenlour David <eivenlou@ualberta.ca>
2020-07-25 19:41:28 -0600

    fix: bottom nav icons (#102)
    * feat: add get started intro
    
    * fix: bottom nav icons

---
+++ lib/icons/aspire_icons.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/widgets.dart';
+
+class AspireIcons {
+  AspireIcons._();
+
+  static const fontFam = 'Aspire';
+  static const fontPkg = null;
+
+  static const IconData pairings
+    = IconData(0xe800, fontFamily: fontFam, fontPackage: fontPkg);
+  static const IconData profile
+    = IconData(0xe801, fontFamily: fontFam, fontPackage: fontPkg);
+  static const IconData chat
+    = IconData(0xe804, fontFamily: fontFam, fontPackage: fontPkg);
+}
