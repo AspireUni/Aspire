76ec39906971568f0bf58b48a587cb393073d559
Helen Gezahegn <gezahegn@ualberta.ca>
2020-07-31 20:04:47 -0600

    feat: pairings page

--- lib/constants/pairings_constants.dart
+++ lib/constants/pairings_constants.dart
@@ -1,9 +1,6 @@
-const String pairingsHeaderTitle = "Pairings";
-const String pairingsSubtitle = "Browse your matches";
+const String readMoreAction = 'Read more';
 
-List<String> dummyImages = const [
-  "images/sign_up/ano_hands_side.png",
-  "images/sign_up/ano_hands_straight_up.png",
-  "images/sign_up/ano_hands_up.png",
-  "images/sign_up/ano_hands_side.png",
-];
+const dummyCard = {
+  "name": "Helen Gezahegn", 
+  "description": """Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas suscipit fringilla purus, sed euismod odio. Nullam quam odio, ultrices at mi id, ornare lobortis diam. Quisque ultricies sem et magna rhoncus accumsan."""
+};
--- lib/pairings/pairings.dart
+++ lib/pairings/pairings.dart
@@ -1,48 +1,138 @@
 import 'package:flutter/material.dart';
 
+import '../common/common.dart';
 import '../common/global_header.dart';
+import '../constants/constants.dart';
 
 class Pairings extends StatelessWidget {
   const Pairings({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    var screenWidth = MediaQuery.of(context).size.width;
+    var screenHeight = MediaQuery.of(context).size.height;
+
     return Scaffold(
       appBar: GlobalHeader(),
       backgroundColor: Colors.white,
-      body: buildPairingsView(context)
-    );
-  }
-
-
-  Widget buildAno(screenWidth, screenHeight) {
-    return Container(
-      width: screenWidth,
-      child: Column(
-        children: <Widget>[
-          Image.asset(
-            'images/ano_mountains.png',
-            height: screenHeight * 0.25,
-          ),
-          Container(
-            height: 1.5,
-            color: Colors.black
-          )
+      body: Column(
+        children: [
+          buildAno(screenHeight),
+          Align(
+            alignment: Alignment.center, 
+            child: buildCardContainer(context, screenWidth, screenHeight)
+          ), 
+          buildReadMoreButton()
         ]
       )
     );
   }
 
-  buildPairingsView(BuildContext context) {
-    var screenWidth = MediaQuery.of(context).size.width;
-    var screenHeight = MediaQuery.of(context).size.height;
-    return Column(
-      children: <Widget>[
-        Positioned(
-          top: screenHeight * 0.15,
-          child: buildAno(screenWidth, screenHeight)
-        ),
+
+  Image buildAno(screenHeight) {
+    return Image.asset(
+      'images/ano_mountains.png',
+      height: screenHeight * 0.25
+    );
+  }
+
+  buildReadMoreButton() {
+    return PrimaryButton(
+      isLight: true,
+      text: readMoreAction,
+      onPressed: () => print("Read more pressed")
+    );
+  }
+
+  Row buildCardHeader(context, screenWidth, screenHeight) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Container(
+          width: screenWidth * 0.10, 
+          height: screenHeight * 0.10,
+          decoration: BoxDecoration(
+            color: Colors.grey[200], 
+            shape: BoxShape.circle
+          )
+        ), 
+        Padding(
+          padding: EdgeInsets.only(top: screenHeight * 0.03), 
+          child: FormatText(
+            text: dummyCard["name"],
+            fontSize: 22.0
+          )
+        )
       ]
     );
   }
+
+  FormatText buildCardBody(context, screenWidth, screenHeight) {
+    return FormatText(
+      text: dummyCard["description"], 
+      fontSize: 14.0,
+      fontWeight: FontWeight.w300,
+      fontHeight: 1.6
+    );
+  }
+  
+  Row buildCardFooter(context, screenWidth, screenHeight) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        SizedBox(
+          width: screenWidth * 0.20, 
+          height: screenHeight * 0.10, 
+          child: FlatButton(
+            child: Image.asset("images/right_arrow.png"), 
+            onPressed: () => print("Arrow pressed")
+          )
+        ), 
+        IconButton(
+          icon: Icon(Icons.favorite),
+          iconSize: screenHeight * 0.03, 
+          color: Theme.of(context).accentColor,
+          onPressed: () => print("Heart pressed"),
+        )
+      ]
+    );
+  }
+
+  Widget buildCard(context, screenWidth, screenHeight) {
+    var sidePadding = screenWidth * 0.05;
+    return Padding(
+      padding: EdgeInsets.only(
+        top: screenHeight * 0.02, 
+        left: sidePadding, 
+        right: sidePadding
+      ),
+      child: Column(
+        children: [
+          buildCardHeader(context, screenWidth, screenHeight),
+          buildCardBody(context, screenWidth, screenHeight), 
+          buildCardFooter(context, screenWidth, screenHeight)
+        ]
+      )
+    );
+  }
+
+  Container buildCardContainer(context, screenWidth, screenHeight) {
+    return Container(
+      width: screenWidth * 0.80,
+      height: screenHeight * 0.42,
+      decoration: BoxDecoration(
+        border: Border.all(
+          color: Theme.of(context).accentColor,
+          width: 1.0,
+          style: BorderStyle.solid
+        ),
+        color: Colors.transparent,
+        shape: BoxShape.rectangle, 
+        borderRadius: BorderRadius.all(Radius.circular(20))
+      ), 
+      child: buildCard(context, screenWidth, screenHeight)
+    );
+  }
+
 }
