44c74a9764588546d342f9dbf5471a35285f2ca0
Helen Gezahegn <gezahegn@ualberta.ca>
2020-08-09 17:13:50 -0600

    feat: add cards animation (#131)
    - move ano to bottom and make it slide up
    - add slide left and right animations to cards
    - move cards outside of pairings into its own `Cards` widget

--- lib/constants/pairings_constants.dart
+++ lib/constants/pairings_constants.dart
@@ -1,6 +1,12 @@
 const String readMoreButtonText = 'Read more';
-
-const dummyCard = {
+const String dummySummary = """Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas suscipit fringilla purus, sed euismod odio. Nullam quam odio, ultrices at mi id, ornare lobortis diam. Quisque ultricies sem et magna rhoncus accumsan.""";
+const dummyCard = [
+  {
   "name": "Helen Gezahegn", 
-  "description": """Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas suscipit fringilla purus, sed euismod odio. Nullam quam odio, ultrices at mi id, ornare lobortis diam. Quisque ultricies sem et magna rhoncus accumsan."""
-};
+  "description": dummySummary
+  },
+  {
+  "name": "Lorem Ipsum", 
+  "description": dummySummary
+  }
+];
